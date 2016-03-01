633ea5580e41c225d23578c7ed70f0f1691ce412
Jon Gauthier <jgauthie@stanford.edu>
2016-02-29 18:53:21 -0800

    Init with heavily-modified version of Theano, forked from 6a3b192

---
+++ generated_version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM THEANO SETUP.PY
+short_version = '0.8.0.dev0'
+version = '0.8.0.dev0'
+git_revision = '6a3b192d37b9c5616ce85a0a3d121bb61fc7aa21'
+full_version = '0.8.0.dev0.dev-%(git_revision)s' % {
+    'git_revision': git_revision}
+release = False
+
+if not release:
+    version = full_version
